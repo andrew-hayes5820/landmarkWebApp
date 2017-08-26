fed814be0dfeb9e974d437dc182605237e0d095a
Timothy Batty <tbatty7@gmail.com>
2017-08-26 09:20:37 -0400

    Adding service back for merge

---
+++ public/mapService.js
@@ -0,0 +1,26 @@
+var app = angular.module('landmarkModule');
+
+app.service('MapService', function($q) {
+    
+    this.init = function(mapLat, mapLng) {
+        var options = {
+            center: new google.maps.LatLng(mapLat, mapLng),  // This sets the center of the map
+            zoom: 11,
+            disableDefaultUI: true    
+        }
+        this.map = new google.maps.Map(
+            document.getElementById("locationMap"), options  // This creates the map
+        );
+        this.places = new google.maps.places.PlacesService(this.map); // I think this can be commented out.
+    }
+
+    this.addMarker = function(coords) {
+        // if(this.marker) this.marker.setMap(null);  // This deletes [previous markers]
+        this.marker = new google.maps.Marker({
+            map: this.map,
+            position: coords, //  {lat: 42.3222402, lng: -83.18128769999998} = This is all that is needed for a marker to be placed
+            animation: google.maps.Animation.DROP
+        });
+        // this.map.setCenter(coords);  // resets center of map
+    }
+});
